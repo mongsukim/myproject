fab9f930bfa9eef78de2c616b63461bf66ad4666
DESKTOP-76PA4NP\hyesu <developermong2@gmail.com>
2024-07-28 16:10:34 +0900

    CSS : Todolist페이지에 css 적용

--- src/components/views/TodoListPage.tsx
+++ src/components/views/TodoListPage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @atlaskit/design-system/ensure-design-token-usage */
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -17,21 +19,28 @@ const TodoListPage = () => {
 
   return (
     <div>
-      TodoListPage
-      <div>
+      <div className=" mt-[100px] w-[700px] mx-auto outline outline-1 outline-gray-500">
+        <div className="flex py-[10px] justify-center border border-b-[1px] border-solid ">
+          <div className="w-1/5 flex justify-center">완료 여부</div>
+          <div className="w-4/5 flex justify-center">할 일</div>
+        </div>
         {todos?.map((item, index) => (
-          <div key={index} className="flex items-center">
-            {item.completed ? (
-              <div>
-                <img src="/images/checkbox-blank-line.png" alt="완료" />
-              </div>
-            ) : (
-              <div>
-                <img src="/images/checkbox-line.png" alt="미완료" />
-              </div>
-            )}
-            &nbsp;|&nbsp;
-            {item.title}
+          <div
+            key={index}
+            className="flex justify-center border border-b-[1px] border-solid py-[5px] "
+          >
+            <div className="w-1/5 flex justify-center">
+              {item.completed ? (
+                <div>
+                  <img src="/images/checkbox-blank-line.png" alt="완료" />
+                </div>
+              ) : (
+                <div>
+                  <img src="/images/checkbox-line.png" alt="미완료" />
+                </div>
+              )}
+            </div>
+            <div className="w-4/5">{item.title}</div>
           </div>
         ))}
       </div>
